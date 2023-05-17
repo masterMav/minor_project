41192d18f9ec8a4c86874b8f2c640c4bd3c97fcc
masterMav <mavdoesdev@gmail.com>
2023-05-18 02:35:31 +0530

    home page finished

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,7 @@
-import TreeImage from "../images/trees.png";
+import cfLogo from "../images/cflogo.png";
+import atcoder from "../images/atcoder.png";
+import cc from "../images/cc.png";
+import icpc from "../images/icpc.png";
 import { useHistory } from "react-router-dom";
 
 const Home = () => {
@@ -12,33 +15,34 @@ const Home = () => {
       <div className="showcase">
         <h6 className=" mb-3 text-muted text-center">Legend has it,</h6>
         <p className="text-center fw-bold theQuote">
-          Happiness is not the absence of problems; it's the ability to deal
-          with them.
+          We are what we repeatedly do. Excellence, then, is not an act, but a
+          habit.
         </p>
         <h6 className="mt-1 text-muted text-center">
-          and maybe I have a bunch of them for you to work on with a nice coffee
-          on a good sunday evening. So Start now to atleast see something new
-          today.
+          Join UIT's first-ever club dedicated to competitive programming. Sign
+          up, compete with friends based on ratings and track your practice.
+          Experience a visual representation of your progress for enhanced
+          learning and growth.
         </h6>
         <div className="text-center">
           <button
             type="button"
-            className="mt-3 btn col-3 btn-lg text-light headButton"
+            className="mt-4 btn col-3 btn-lg text-light headButton"
             onClick={handleClick}
           >
             Start here
           </button>
         </div>
       </div>
-      <img src={TreeImage} alt="treeImage" className="treeImage mt-5 pt-5" />
+      <img src={cfLogo} alt="cflogo" className="cfLogo mt-5 pt-5" />
+      <img src={atcoder} alt="atcoder" className="atcoder mt-5 pt-5" />
+      <img src={cc} alt="cc" className="cc mt-5 pt-5" />
+      <img src={icpc} alt="icpc" className="icpc mt-5 pt-5" />
       <div className="text-center text-muted homeFooter">
         <div>
-          Made with ❤️ by Manavkumar Patel.
-          <a href="https://github.com/masterMav/finn">
+          Made by Jayant, Lakshya and Manav.
+          <a href="https://github.com/masterMav/minor_project">
             <p className="fa-brands fa-square-github ms-4 me-2 fs-4"></p>
-          </a>
-          <a href="https://www.linkedin.com/in/manavkumar-patel-69802b202/">
-            <p className="fa-brands fa-linkedin fs-4"></p>
           </a>
         </div>
       </div>
